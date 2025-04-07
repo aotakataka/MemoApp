febd531af36ff77fce671591370722dfd95de967
aotaka <130732987+aotakataka@users.noreply.github.com>
2025-04-08 00:03:16 +0900

    Button Styling

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -44,8 +44,8 @@ const Index = (): JSX.Element => {
                 </View>
 
             </View>
-            <View>
-                <Text>+</Text>
+            <View style={styles.circleButton}>
+                <Text style = {styles.circleButtonLabel}>+</Text>
             </View>
         </View>
     )
@@ -95,6 +95,27 @@ const styles = StyleSheet.create({
         fontSize: 12,
         lineHeight: 16,
         color: '#848484'
+    },
+    circleButton: {
+        width: 64,
+        height: 64,
+        borderRadius: 32,
+        backgroundColor: '#467FD3',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        bottom: 40,
+        right: 40,
+        shadowColor: '#000000',
+        shadowOpacity: 0.25,
+        shadowRadius: 8,
+        shadowOffset: {width: 0, height: 8},
+        elevation: 8
+    },
+    circleButtonLabel: {
+        color: '#ffffff',
+        fontSize: 40,
+        lineHeight: 48
     }
 })
 
