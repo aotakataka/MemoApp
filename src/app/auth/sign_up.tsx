c6597b7e28198c8e3d4f6f64976e67e1ad8ad495
aotaka <130732987+aotakataka@users.noreply.github.com>
2025-04-08 19:31:18 +0900

    Interaction between SignUp and LogIn

--- src/app/auth/sign_up.tsx
+++ src/app/auth/sign_up.tsx
@@ -1,5 +1,7 @@
 import {View, Text, TextInput, TouchableOpacity, StyleSheet} from 'react-native'
 
+import { Link } from 'expo-router'
+
 import Header from '../../components/Header'
 import Button from '../../components/Button'
 
@@ -14,9 +16,11 @@ const SignUp = (): JSX.Element => {
                 <Button label='Submit' />
                 <View  style = {styles.footer}>
                     <Text style = {styles.footerText}>Already registered?</Text>
-                    <TouchableOpacity>
-                        <Text style = {styles.footerLink}>Log In</Text>
-                    </TouchableOpacity>
+                    <Link href='/auth/log_in' asChild>
+                        <TouchableOpacity>
+                            <Text style = {styles.footerLink}>Log In</Text>
+                        </TouchableOpacity>
+                    </Link>
                 </View>
             </View>
         </View>
